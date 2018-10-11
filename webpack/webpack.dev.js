b4c2e2ce5a40ad73145719c6279d3cf632603aa5
Brian <bzchen93@gmail.com>
2018-10-11 20:41:15 +1100

    fix dev mode auto reload to be stable

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,27 +1,20 @@
 const path = require('path');
-const webpack = require('webpack');
 const merge = require('webpack-merge');
-const EventHooksPlugin = require('event-hooks-webpack-plugin');
-const rimraf = require('rimraf');
 
 const common = require('./../webpack.config');
 
 const config = merge(common, {
   mode: 'development',
+  output: {
+    filename: 'index.js',
+  },
   devtool: 'inline-source-map',
-  plugins: [
-    new EventHooksPlugin({
-      watchRun: () => {
-        rimraf(path.join(__dirname, '../public/dist'), {}, () => {});
-        webpack(config).run();
-      },
-    }),
-  ],
   devServer: {
     contentBase: path.join(__dirname, '../public'),
     watchContentBase: true,
     port: '3000',
     open: true,
+    overlay: true,
     historyApiFallback: {
       rewrites: [
         { from: /./, to: 'index.html' },
