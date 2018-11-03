a34ca97939f101b4a193490257a8e3fb851f80a6
Brian Chen <brian.chen@tabcorp.com.au>
2018-11-03 16:15:52 +1100

    update lint

--- babel.config.js
+++ babel.config.js
@@ -16,11 +16,11 @@ const config = {
       'transform-imports',
       {
         lodash: {
-          transform: 'lodash/${member}',
+          transform: 'lodash/${member}', // eslint-disable-line no-template-curly-in-string
           preventFullImport: true,
         },
         'react-router': {
-          transform: 'react-router/${member}',
+          transform: 'react-router/${member}', // eslint-disable-line no-template-curly-in-string
           preventFullImport: true,
         },
       },
